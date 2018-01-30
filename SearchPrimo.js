7a7256a0e1d56baf59118830e2f4248d9100fb11
andrewpress <35976292+andrewpress@users.noreply.github.com>
2018-01-31 09:24:23 +1100

    Create SearchPrimo.js

---
+++ SearchPrimo.js
@@ -0,0 +1,3 @@
+javascript:(function(){
+  window.open('https://primo.csu.edu.au/discovery/search?search_scope=MyInst_and_CI&sortby=rank&vid=61CSU_INST:61CSU&offset=0&query=any,contains,'+encodeURIComponent(window.getSelection()));
+  })();
